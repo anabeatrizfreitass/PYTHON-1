cdd79e2696867dfbbafbc3c7219bbef426569206
anabeatrizfreitass <anabeatrizfreitas771@gmail.com>
2024-09-12 12:08:56 -0300

    Create notamedia2.py

---
+++ notamedia2.py
@@ -0,0 +1,9 @@
+arquivo 2. notamedia2.py
+#Feito pelo aluno Gustavo
+print('Coloque suas notas')
+n1 = float(input("nota 1: "))
+n2 = float(input("nota 2: "))
+n3 = float(input("nota 3: "))
+n4 = float(input("nota 4: "))
+n5 = float(input("nota 5: "))
+mídia = (n1+n2+n3+n4+n
